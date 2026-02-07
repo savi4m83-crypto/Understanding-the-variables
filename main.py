7201b3dc65b2db26954a68eb513e2e0998b1711a
savi4m83-crypto <savi4m83@gmail.com>
2026-02-06 18:49:42 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+name="Muzzamil"
+gender="Boy"
+print("My name is",name)
+print("My gender is",gender)
+print("My name is",name,"My gender is",gender)
+print("My name is",name,"and My gender is",gender)
